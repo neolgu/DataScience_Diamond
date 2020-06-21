a4052f04401dc33fa79843af000f0a9b6c3d17aa
neolgu <32592676+neolgu@users.noreply.github.com>
2020-06-21 17:37:34 +0900

    Add missing value handler
    Add missing value handler

--- DS_TT/K-Nearest_Neighbors.py
+++ DS_TT/K-Nearest_Neighbors.py
@@ -5,13 +5,14 @@ from sklearn.neighbors import KNeighborsClassifier
 
 data = pd.read_csv("diamonds_KNN.csv", index_col=0)
 
+# Split X, y
 X = data.drop(['price'], axis='columns', inplace=False)
 y = data['price']
 
-kfold = KFold(5, shuffle=True)
 
+kfold = KFold(5, shuffle=True)
 gcv = GridSearchCV(KNeighborsClassifier(),
-                   param_grid={'n_neighbors': np.arange(1, 10, 1),
+                   param_grid={'n_neighbors': np.arange(1, 10, 2),
                                'p': [1, 2]},
                    cv=kfold,
                    scoring='f1_micro',
@@ -20,6 +21,3 @@ gcv.fit(X, y)
 
 print('final params: ', gcv.best_params_)
 print('best score: ', gcv.best_score_)
-print("{}".format(gcv.best_estimator_))
-
-# best n = 5
--- DS_TT/data_preprocessing.py
+++ DS_TT/data_preprocessing.py
@@ -3,11 +3,17 @@ import numpy as np
 import sklearn.preprocessing as pp
 from sklearn.compose import ColumnTransformer
 
-data = pd.read_csv("diamonds.csv")
-data.drop(['num'], axis='columns', inplace=True)
+data = pd.read_csv("diamonds_masked.csv", index_col=0)
 
 # Drop columns
 data.drop(['depth', 'table'], axis='columns', inplace=True)
+
+
+# Nan value handling
+data.dropna(axis=0, inplace=True)
+data.reset_index(drop=True, inplace=True)
+# data.fillna(method='bfill', inplace=True)
+
 
 # VER. Multiple Regression
 """
@@ -28,8 +34,9 @@ print(data_transformed.head())
 data_transformed.to_csv("diamonds_MR.csv")
 """
 
+
 # VER. K-NN
-"""
+""""""
 num_attr = ['carat', 'x', 'y', 'z']
 transformer = ColumnTransformer([
     ('num', pp.StandardScaler(), num_attr)  # numeric standard scaling
@@ -48,32 +55,36 @@ data_labeled['clarity'] = (pp.LabelEncoder().fit_transform(data['clarity']))
 # concat dataframe
 data_transformed = pd.concat([data_transformed, data_labeled], axis=1)
 
+
 # Data range
 label_range = [x for x in range(0, 20001, 2000)]  # divide
-# print(label_range)
 
 price_label = pd.DataFrame()
-price_label['price'] = data['price']
+price_label['price'] = data.dropna()['price']
+
 for i in range(len(label_range) - 1):
     price_label['price'] = price_label['price'].apply(lambda x: i if label_range[i] < x <= label_range[i + 1] else x)
+
 
 # concat dataframe
 data_transformed = pd.concat([data_transformed, price_label], axis=1)
 
-# num_attr = ['cut', 'color', 'clarity']
-# transformer = ColumnTransformer([
-#     ('num', pp.StandardScaler(), num_attr)  # numeric standard scaling
-# ])
-# new_data = transformer.fit_transform(data_transformed)
-# 
-# new_data = pd.DataFrame(new_data)
-# new_data.rename(columns={0: 'cut', 1: 'color', 2: 'clarity'}, inplace=True)
-# 
-# data_transformed['cut'] = new_data['cut']
-# data_transformed['color'] = new_data['color']
-# data_transformed['clarity'] = new_data['clarity']
+
+# categorical value scaling
+num_attr = ['cut', 'color', 'clarity']
+transformer = ColumnTransformer([
+    ('num', pp.StandardScaler(), num_attr)  # numeric standard scaling
+])
+new_data = transformer.fit_transform(data_transformed)
+
+new_data = pd.DataFrame(new_data)
+new_data.rename(columns={0: 'cut', 1: 'color', 2: 'clarity'}, inplace=True)
+
+data_transformed['cut'] = new_data['cut']
+data_transformed['color'] = new_data['color']
+data_transformed['clarity'] = new_data['clarity']
+
 
 print(data_transformed.head(5))
-
 data_transformed.to_csv("diamonds_KNN.csv")
-"""
+""""""
